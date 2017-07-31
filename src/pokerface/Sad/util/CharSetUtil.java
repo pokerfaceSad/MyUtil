93980d01678c298f0c094072cdff097ee6b76b33
pokerfaceSad <pokerfacesad@126.com>
2017-07-31 23:33:05 +0800

    add charsetUtil

---
+++ src/pokerface/Sad/util/CharSetUtil.java
@@ -0,0 +1,55 @@
+package pokerface.Sad.util;
+
+import java.lang.Character.UnicodeBlock;
+
+import sun.nio.cs.UnicodeEncoder;
+
+public class CharSetUtil {
+	
+	public static void main(String[] args) {
+		
+		String unicodeStr = "\\u7a0b\\u9ad8\\u5cf0";
+		System.out.println(unicode2String(unicodeStr));
+		System.out.println(string2Unicode("程高峰"));
+	}
+	
+	/**
+	 * 字符串转换unicode
+	 */
+	public static String string2Unicode(String string) {
+	 
+	    StringBuffer unicode = new StringBuffer();
+	 
+	    for (int i = 0; i < string.length(); i++) {
+	 
+	        // 取出每一个字符
+	        char c = string.charAt(i);
+	 
+	        // 转换为unicode
+	        unicode.append("\\u" + Integer.toHexString(c));
+	    }
+	 
+	    return unicode.toString();
+	}
+	
+	/**
+	 * unicode 转字符串
+	 */
+	public static String unicode2String(String unicode) {
+	 
+	    StringBuffer string = new StringBuffer();
+	 
+	    String[] hex = unicode.split("\\\\u");
+	 
+	    for (int i = 1; i < hex.length; i++) {
+	 
+	        // 转换出每一个代码点
+	        int data = Integer.parseInt(hex[i], 16);
+	 
+	        // 追加成string
+	        string.append((char) data);
+	    }
+	 
+	    return string.toString();
+	}
+}
